f225b307bf78043cb676403f6b2a0c40842a6ca3
HAZEL <hyezico@outlook.com>
2019-03-20 01:27:35 -0400

    mobile css

--- bg-transition.js
+++ bg-transition.js
@@ -1,30 +1,41 @@
-const bcBackground = document.getElementById("home-title-BC");
-const adBackground = document.querySelector(".home-title-AD");
-const background = document.querySelector(".background");
-const fadeOutContainer = document.querySelector(".fade-out-container");
+const bcBackground = document.getElementById('home-title-BC');
+const adBackground = document.querySelector('.home-title-AD');
+const background = document.querySelector('.background');
+const fadeOutContainer = document.querySelector('.fade-out-container');
+const filter = 'win16|win32|win64|mac';
 
-function gradientTransitionBC () {
-    background.classList.add("background-transition-BC");
-    //redirect to blockchain
-    setTimeout(() => {
-      fadeOutContainer.classList.add("fade-out");
-    }, 800);
-    setTimeout(() => {
-      window.location.href = 'blockchain.html'
-    }, 1200);
-}
+function gradientTransitionBC() {
+  if (navigator.platform) {
+    if (0 > filter.indexOf(navigator.platform.toLowerCase())) {
+      window.location.href = 'blockchain.html';
+      return;
+    }
+  }
 
-function gradientTransitionAD () {
-  background.classList.add("background-transition-AD");
+  background.classList.add('background-transition-BC');
   //redirect to blockchain
   setTimeout(() => {
-    fadeOutContainer.classList.add("fade-out");
+    fadeOutContainer.classList.add('fade-out');
   }, 800);
   setTimeout(() => {
-    window.location.href = 'artdomain.html'
+    window.location.href = 'blockchain.html';
   }, 1200);
 }
 
-bcBackground.addEventListener("click", gradientTransitionBC);
-adBackground.addEventListener("click", gradientTransitionAD);
+function gradientTransitionAD() {
+  if (0 > filter.indexOf(navigator.platform.toLowerCase())) {
+    window.location.href = 'artdomain.html';
+    return;
+  }
+  background.classList.add('background-transition-AD');
+  //redirect to blockchain
+  setTimeout(() => {
+    fadeOutContainer.classList.add('fade-out');
+  }, 800);
+  setTimeout(() => {
+    window.location.href = 'artdomain.html';
+  }, 1200);
+}
 
+bcBackground.addEventListener('click', gradientTransitionBC);
+adBackground.addEventListener('click', gradientTransitionAD);
